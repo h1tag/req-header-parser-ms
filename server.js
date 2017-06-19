3a7987f5be9faea5b0911299d2167881caa44a6d
h1tag <kkerm@protonmail.com>
2017-06-19 11:27:08 +0300

    deleted the log messages

--- server.js
+++ server.js
@@ -2,10 +2,6 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-	console.log(process.platform);
-	console.log(req.headers);
-	console.log(req.ip);
-
 	var ip = req.ip;
 	var lang = req.headers["accept-language"];
 	var os = process.platform;
